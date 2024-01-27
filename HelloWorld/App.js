ca50ac7d26db13a5ced2ba7c56a05a672e7f15b2
fatmaerciyas <fatmaerciyas22@gmail.com>
2024-01-27 15:14:11 +0300

    created project

---
+++ HelloWorld/App.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
+      <Text style={{ color: "white", fontSize: 32 }}>Hello</Text>
+    </View>
+  );
+}
